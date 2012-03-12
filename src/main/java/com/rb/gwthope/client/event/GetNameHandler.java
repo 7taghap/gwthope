24da0c9815c7bc8eaca4a2da85784fd43acbf298
rey <caburnay_rb@yahoo.com>
2012-03-12 16:45:35 +0800

    working debug and mvp sample layout

--- src/main/java/com/rb/gwthope/client/event/GetNameHandler.java
+++
@@ -1,9 +0,0 @@
-package com.rb.gwthope.client.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface GetNameHandler extends EventHandler{
-	
-	void onGetName(GetNameEvent event);
-
-}
